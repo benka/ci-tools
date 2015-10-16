03f1c6a390f6ab1c33851990cdc0a3e39b823ca8
benka <andrew.benkoczi@gmail.com>
2015-10-16 09:05:49 -0400

    init

--- jira-automator/lib/jira-automator/automator.rb
+++ jira-automator/lib/jira-automator/automator.rb
@@ -8,12 +8,11 @@ module JiraAutomator
 
         desc "get-filters", "gets filters from jira"
         def get_filters
-            puts "get filters"
-           ##uri = URI('https://thesib.atlassian.net/rest/api/2/filter/favourite')
-            #res = Net::HTTP.get_response(uri)
-           # puts res.code
-            #puts res.message
-           # puts res.body
+           uri = URI('https://thesib.atlassian.net/rest/api/2/filter/favourite')
+           res = Net::HTTP.get_response(uri)
+           puts res.code
+           puts res.message
+           puts res.body
         end
     end
 end
