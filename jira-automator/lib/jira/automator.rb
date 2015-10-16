499abdd9e92cc19186c29d906a00517c5894f6c9
benka <andrew.benkoczi@gmail.com>
2015-10-16 08:54:32 -0400

    init 2

--- jira-automator/lib/jira/automator.rb
+++ jira-automator/lib/jira/automator.rb
@@ -5,7 +5,7 @@ require "net/http"
 module Jira
     class Automator
 
-        def run()
+        def run
 
            ##uri = URI('https://thesib.atlassian.net/rest/api/2/filter/favourite')
             #res = Net::HTTP.get_response(uri)
