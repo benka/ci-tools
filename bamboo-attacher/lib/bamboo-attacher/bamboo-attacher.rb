70886bf2ac074f68f483effe73c667dca4b67339
benka <andrew.benkoczi@gmail.com>
2015-10-27 13:27:31 -0400

    0.0.3 working commands

--- bamboo-attacher/lib/bamboo-attacher/bamboo-attacher.rb
+++ bamboo-attacher/lib/bamboo-attacher/bamboo-attacher.rb
@@ -14,17 +14,15 @@ module BambooAttacher
 
     class Attacher < Thor
 
-        desc "get-buildplans", "gets build plans"
+        desc "get-plans", "gets build plans"
         option :user, :type => :string, :required => true
         option :pwd, :type => :string, :required => true
-        def get_buildplans
-            #uri = URI('https://thesib.atlassian.net/builds/rest/api/latest/plan/TEST-TEST')
+        def get_plans
             uri = URI('https://thesib.atlassian.net/builds/rest/api/latest/plan')
             puts "URI: #{uri}"
 
             r = Resources::Request.new(uri, options[:user], options[:pwd])
             req=r.create_get_request_header
-            #req=r.create_post_request_header(false)
 
             res = Net::HTTP.start(uri.hostname,
                 :use_ssl => uri.scheme == 'https'
@@ -54,17 +52,16 @@ module BambooAttacher
             end
         end
 
-        desc "get-buildplan", "gets build plans from a specific project"
+        desc "get-plan_bykey", "gets build plans from a specific project"
         option :user, :type => :string, :required => true
         option :pwd, :type => :string, :required => true
         option :plan_key, :type => :string, :required => true
-        def get_buildplan
+        def get_plan_bykey
             uri = URI("https://thesib.atlassian.net/builds/rest/api/latest/plan/#{options[:plan_key]}/")
             puts "URI: #{uri}"
 
             r = Resources::Request.new(uri, options[:user], options[:pwd])
             req=r.create_get_request_header
-            #req=r.create_post_request_header(false)
 
             res = Net::HTTP.start(uri.hostname,
                 :use_ssl => uri.scheme == 'https'
@@ -82,24 +79,24 @@ module BambooAttacher
             end
         end
 
-        desc "run-buildplan", "runs a specific build plan"
+        desc "run-build", "runs a specific build plan"
         option :user, :type => :string, :required => true
         option :pwd, :type => :string, :required => true
         option :build_key, :type => :string, :required => true
-        option :stage, :type => :string, :required => false, :default => false
         option :build_number, :type => :string, :required => false, :default => false
-        def run_buildplan
+        option :all_stages, :type => :boolean, :required => false, :default => false
+        def run_build
             key = options[:build_key]
-            #post = 
             key = "#{key}-#{options[:build_number]}" if options[:build_number]
-            key = "#{key}?executeAllStages=true" if options[:stage]
-            key = "#{key}&os_authType=basic"
-            #key = "#{key}?stage=#{options[:stage]}&executeAllStages=true" if options[:stage]
+            key = "#{key}?executeAllStages=#{options[:all_stages]}" if options[:all_stages]
             uri = URI("https://thesib.atlassian.net/builds/rest/api/latest/queue/#{key}")
             puts "URI: #{uri}"
 
+            type = "post"
+            type = "put" if options[:build_number]
+
             r = Resources::Request.new(uri, options[:user], options[:pwd])
-            req=r.create_post_request_header(false)
+            req = r.create_request_header(type, false)
 
             res = Net::HTTP.start(uri.hostname,
                 :use_ssl => uri.scheme == 'https'
@@ -113,8 +110,41 @@ module BambooAttacher
             else 
                 puts res.body
             end
+        end
 
+        desc "run-build-stage", "runs a specific stage in a specific build plan"
+        option :user, :type => :string, :required => true
+        option :pwd, :type => :string, :required => true
+        option :build_key, :type => :string, :required => true
+        option :stage, :type => :string, :required => false, :default => false
+        option :build_number, :type => :string, :required => false, :default => false
+        def run_build_stage
+            key = options[:build_key]
 
+            key = "#{key}-#{options[:build_number]}" if options[:build_number]
+            key = "#{key}?stage=#{options[:stage]}" if options[:stage]
+
+            uri = URI("https://thesib.atlassian.net/builds/rest/api/latest/queue/#{key}")
+            puts "URI: #{uri}"
+
+            type = "post"
+            type = "put" if options[:build_number]
+
+            r = Resources::Request.new(uri, options[:user], options[:pwd])
+            req = r.create_request_header(type, false)
+
+            res = Net::HTTP.start(uri.hostname,
+                :use_ssl => uri.scheme == 'https'
+            ) { |http|
+                http.request(req)
+            }
+
+            if res.code != "200"
+                puts res.code
+                puts res.message
+            else 
+                puts res.body
+            end
         end
     end
 end
--- bamboo-attacher/lib/bamboo-attacher/res/request.rb
+++ bamboo-attacher/lib/bamboo-attacher/res/request.rb
@@ -17,14 +17,29 @@ module BambooAttacher
                 return req
             end
 
+            def create_request_header(type, post)
+                req = create_post_request_header(post) if type == "post"
+                req = create_put_request_header(post) if type == "put"
+                return req
+            end
+
             def create_post_request_header(post)
+                #puts "Creating POST header"
                 req = Net::HTTP::Post.new(@uri)
                 req.basic_auth @user, @pwd
                 req.content_type = 'application/xml'
-                #req.add_field 'X-Atlassian-Token' ,'nocheck'
                 req.body = post if post
                 return req
-            end            
+            end
+
+            def create_put_request_header(post)
+                #puts "Creating PUT header"
+                req = Net::HTTP::Put.new(@uri)
+                req.basic_auth @user, @pwd
+                req.content_type = 'application/xml'
+                req.body = post if post
+                return req
+            end 
         end
     end
 end
